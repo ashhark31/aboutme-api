106b359d13658f839f4ee2cc3b2433cba7caddaf
mohammad ashhar <ashhark31@gmail.com>
2023-12-24 13:24:01 +0530

    next_initial_deployment

--- src/utils/envProperties.js
+++ src/utils/envProperties.js
@@ -1,12 +1,6 @@
 const path = require("path")
 
-// module.exports = class ENVPropertyValues {
-//     static propertiesReader = require("properties-reader")
-//     static properties = ENVPropertyValues.propertiesReader(path.normalize(__dirname + "../../../.env"))
-// }
-
-
 module.exports = class ENVPropertyValues {
     static propertiesReader = require("properties-reader")
-    static properties = ENVPropertyValues.propertiesReader('/app/.env')
+    static properties = ENVPropertyValues.propertiesReader(path.normalize(__dirname + "../../../.env"))
 }
